34f80fd183c92e5606de699b2dc230adfd208546
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-04-30 10:11:58 +0300

    ft: feature for user

---
+++ user.py
@@ -0,0 +1,14 @@
+from platform import java_ver
+
+
+def person():
+    user = {
+        username: 'John',
+        password: '123',
+    
+    }
+    return user
+    
+user["username"] = input("Enter your username: ")
+user["password"] = input("Enter your password: ")
+
